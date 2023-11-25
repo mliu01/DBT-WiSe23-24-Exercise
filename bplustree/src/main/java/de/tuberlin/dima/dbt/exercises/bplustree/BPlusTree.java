d6093edfe757d7d5c063afd92cfe8d128bbbf736
Ankit Chaudhary <ankit.chaudhary@tu-berlin.de>
2023-11-25 20:38:56 +0100

    Initial Commit

---
+++ bplustree/src/main/java/de/tuberlin/dima/dbt/exercises/bplustree/BPlusTree.java
@@ -0,0 +1,117 @@
+package de.tuberlin.dima.dbt.exercises.bplustree;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * Implementation of a B+ tree.
+ * <p>
+ * The capacity of the tree is given by the capacity argument to the
+ * constructor. Each node has at least {capacity/2} and at most {capacity} many
+ * keys. The values are strings and are stored at the leaves of the tree.
+ * <p>
+ * For each inner node, the following conditions hold:
+ * <p>
+ * {pre}
+ * Integer[] keys = innerNode.getKeys();
+ * Node[] children = innerNode.getChildren();
+ * {pre}
+ * <p>
+ * - All keys in {children[i].getKeys()} are smaller than {keys[i]}.
+ * - All keys in {children[j].getKeys()} are greater or equal than {keys[i]}
+ * if j > i.
+ */
+public class BPlusTree {
+
+    ///// Implement these methods
+
+    private LeafNode findLeafNode(Integer key, Node node,
+                                  Deque<InnerNode> parents) {
+        if (node instanceof LeafNode) {
+            return (LeafNode) node;
+        } else {
+            InnerNode innerNode = (InnerNode) node;
+            if (parents != null) {
+                parents.push(innerNode);
+            }
+            // TODO: traverse inner nodes to find leaf node
+            return null;
+        }
+    }
+
+    private String lookupInLeafNode(Integer key, LeafNode node) {
+        // TODO: lookup value in leaf node
+        return null;
+    }
+
+    private void insertIntoLeafNode(Integer key, String value,
+                                    LeafNode node, Deque<InnerNode> parents) {
+        // TODO: insert value into leaf node (and propagate changes up)
+    }
+
+    private String deleteFromLeafNode(Integer key, LeafNode node,
+                                      Deque<InnerNode> parents) {
+        // TODO: delete value from leaf node (and propagate changes up)
+        return null;
+    }
+
+    ///// Public API
+    ///// These can be left unchanged
+
+    /**
+     * Lookup the value stored under the given key.
+     * @return The stored value, or {null} if the key does not exist.
+     */
+    public String lookup(Integer key) {
+        LeafNode leafNode = findLeafNode(key, root);
+        return lookupInLeafNode(key, leafNode);
+    }
+
+    /**
+     * Insert the key/value pair into the B+ tree.
+     */
+    public void insert(int key, String value) {
+        Deque<InnerNode> parents = new LinkedList<>();
+        LeafNode leafNode = findLeafNode(key, root, parents);
+        insertIntoLeafNode(key, value, leafNode, parents);
+    }
+
+    /**
+     * Delete the key/value pair from the B+ tree.
+     * @return The original value, or {null} if the key does not exist.
+     */
+    public String delete(Integer key) {
+        Deque<InnerNode> parents = new LinkedList<>();
+        LeafNode leafNode = findLeafNode(key, root, parents);
+        return deleteFromLeafNode(key, leafNode, parents);
+    }
+
+    ///// Leave these methods unchanged
+
+    private int capacity = 0;
+
+    private Node root;
+
+    public BPlusTree(int capacity) {
+        this(new LeafNode(capacity), capacity);
+    }
+
+    public BPlusTree(Node root, int capacity) {
+        assert capacity % 2 == 0;
+        this.capacity = capacity;
+        this.root = root;
+    }
+
+    public Node rootNode() {
+        return root;
+    }
+
+    public String toString() {
+        return new BPlusTreePrinter(this).toString();
+    }
+
+    private LeafNode findLeafNode(Integer key, Node node) {
+        return findLeafNode(key, node, null);
+    }
+
+}

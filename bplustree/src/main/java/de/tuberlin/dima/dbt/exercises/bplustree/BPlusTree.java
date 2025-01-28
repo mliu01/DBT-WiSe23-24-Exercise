c256218c5e3de2545d05e5b4c88faa8c82d9ab7d
Mandy Liu <mandy.liu@campus.tu-berlin.de>
2025-01-28 16:43:48 +0100

    add solution for find leaf node and insert leaf node for full root

--- bplustree/src/main/java/de/tuberlin/dima/dbt/exercises/bplustree/BPlusTree.java
+++ bplustree/src/main/java/de/tuberlin/dima/dbt/exercises/bplustree/BPlusTree.java
@@ -1,7 +1,6 @@
 package de.tuberlin.dima.dbt.exercises.bplustree;
 
-import java.util.Deque;
-import java.util.LinkedList;
+import java.util.*;
 
 /**
  * Implementation of a B+ tree.
@@ -35,18 +34,184 @@ public class BPlusTree {
                 parents.push(innerNode);
             }
             // TODO: traverse inner nodes to find leaf node
+            int branch = -1;
+
+            Integer[] currentNodeKeys = innerNode.getKeys();
+            for (int i = 0; i< currentNodeKeys.length; i++) {
+                if (currentNodeKeys[i] == null) break;
+                branch = i; // take left child
+                if (currentNodeKeys[i] <= key) {
+                    branch = i+1; // take right child
+                } else {
+                    break;
+                }
+            }
+
+            Node[] children = innerNode.getChildren();
+            if (branch != -1 && branch < children.length) {
+
+                Node child = children[branch];
+                if (child instanceof InnerNode) {
+                    return findLeafNode(key, child, parents);
+                }
+                return (LeafNode) child;
+            }
+
             return null;
         }
     }
 
     private String lookupInLeafNode(Integer key, LeafNode node) {
         // TODO: lookup value in leaf node
+        if (node == null) return null;
+        Integer[] keys = node.getKeys();
+        String[] values = node.getValues();
+
+        for (int i =0; i < keys.length; i++) {
+            if (keys[i] == null) continue;
+
+            if (keys[i].equals(key)) {
+                return values[i];
+            }
+        }
         return null;
     }
 
     private void insertIntoLeafNode(Integer key, String value,
                                     LeafNode node, Deque<InnerNode> parents) {
         // TODO: insert value into leaf node (and propagate changes up)
+        if (node == null) return;
+        if (nodeHasSpace(node)) {
+            fillLeafNode(key, value, node);
+
+        } else { // full node -> split!
+            List<Integer> keys = new ArrayList<>(Arrays.asList(node.getKeys()));
+            List<String> values = new ArrayList<>(Arrays.asList(node.getValues()));
+
+            int insertPos = findInsertPosition(keys, key);
+            keys.add(insertPos, key);
+            values.add(insertPos, value);
+
+            // split node into two with smaller one on the left (1) and bigger one on the right (2)
+            int splitPoint = keys.size() / 2;
+            List<Integer> lKeys = keys.subList(0, splitPoint);
+            List<String> lValues = values.subList(0, splitPoint);
+            List<Integer> rKeys = keys.subList(splitPoint, keys.size());
+            List<String> rValues = values.subList(splitPoint, values.size());
+
+            // Current Node becomes left child
+            node.setKeys(lKeys.toArray(new Integer[0]));
+            node.setValues(lValues.toArray(new String[0]));
+
+            // Create new leaf node for right child node
+            LeafNode rNode = new LeafNode(rKeys.toArray(new Integer[0]), rValues.toArray(new String[0]), BPlusTreeUtilities.CAPACITY);
+            // propagate split to the parents, splitKey is the first key in the right child node
+            propagateToParents(rKeys.get(0), node, rNode, parents);
+        }
+    }
+
+
+    private Boolean nodeHasSpace(Node node) {
+        Integer[] keys = node.getKeys();
+        int counter = 0;
+        for (Integer key : keys) {
+            if (key == null) counter++;
+        }
+        return counter != 0;
+    }
+
+    private void fillLeafNode(Integer key, String value, LeafNode node) {
+        List<Integer> keys = new ArrayList<>(Arrays.asList(node.getKeys()));
+        List<String> values = new ArrayList<>(Arrays.asList(node.getValues()));
+
+        int insertPos = findInsertPosition(keys, key);
+        keys.add(insertPos, key);
+        values.add(insertPos, value);
+
+        node.setKeys(keys.toArray(new Integer[0]));
+        node.setValues(values.toArray(new String[0]));
+    }
+
+    private void fillInnerNode(Integer key, Node parent, Node node) {
+        List<Integer> keys = new ArrayList<>(Arrays.asList(parent.getKeys()));
+        List<Node> pChildren = new ArrayList<>(Arrays.asList(((InnerNode)parent).getChildren()));
+
+        int insertPos = findInsertPosition(keys, key);
+        keys.add(insertPos, key);
+        pChildren.add(insertPos + 1, node);
+
+        parent.setKeys(keys.toArray(new Integer[0]));
+        ((InnerNode)parent).setChildren(pChildren.toArray(new Node[0]));
+    }
+
+    private int findInsertPosition(List<Integer> keys, Integer key) {
+        for (int i = 0; i < keys.size(); i++) {
+            if (keys.get(i) == null || keys.get(i) > key) {
+                return i;
+            }
+        }
+        return keys.size(); // Insert at the end when key bigger than all keys
+    }
+
+    private void propagateToParents(Integer splitKey, Node leftNode, Node rightNode, Deque<InnerNode> parents) {
+        if (parents.isEmpty()) {
+            // new root if there's no parent: split reached the root
+            InnerNode newRoot = new InnerNode(BPlusTreeUtilities.CAPACITY);
+            newRoot.setKeys(new Integer[]{splitKey});
+            newRoot.setChildren(new Node[]{leftNode,rightNode});
+
+            this.root = newRoot;
+            return;
+        }
+
+        // Get the parent node from the stack.
+        InnerNode parent = parents.pop();
+
+        if (nodeHasSpace(parent)) {
+            fillInnerNode(splitKey, parent, rightNode);
+        } else {
+            // Parent is full, so split and propagate further.
+            splitInnerNode(parent, splitKey, rightNode, parents);
+        }
+
+    }
+
+    private void splitInnerNode(InnerNode node, Integer splitKey, Node newChild, Deque<InnerNode> parents) {
+        // Combine keys and children into a single structure.
+        List<Integer> keys = new ArrayList<>(Arrays.asList(node.getKeys()));
+        List<Node> children = new ArrayList<>(Arrays.asList(node.getChildren()));
+
+        int indexToInsert = findInsertPosition(keys, splitKey);
+        keys.add(indexToInsert, splitKey);
+        children.add(indexToInsert + 1, newChild);
+
+        // Split the keys and children.
+        int splitPoint = keys.size() / 2;
+
+        List<Integer> lKeys = keys.subList(0, splitPoint);
+        List<Node> lChildren = children.subList(0, splitPoint + 1);
+        List<Integer> rKeys = keys.subList(splitPoint+1, keys.size());
+        List<Node> rChildren = children.subList(splitPoint+1, children.size());
+
+        if (rKeys.size() != rChildren.size()-1) {
+            splitPoint = splitPoint + 1;
+            rKeys = keys.subList(splitPoint + 1, keys.size());
+            rChildren = children.subList(splitPoint + 1, children.size());
+        }
+
+        // Update the current node with the left half.
+        node.setKeys(lKeys.toArray(new Integer[0]));
+        node.setChildren(lChildren.toArray(new Node[0]));
+
+        // Create a new inner node for the right half.
+        InnerNode newInnerNode = new InnerNode(rKeys.toArray(new Integer[0]), rChildren.toArray(new Node[0]), BPlusTreeUtilities.CAPACITY);
+
+        // Propagate the split key to the parent. // if parents empty, give splitKey -> for new root, if not give rKeys position 0
+        if (parents.isEmpty()) {
+            propagateToParents(keys.get(splitPoint), node, newInnerNode, parents);
+        } else {
+            propagateToParents(rKeys.get(0), node, newInnerNode, parents);
+        }
     }
 
     private String deleteFromLeafNode(Integer key, LeafNode node,
--- bplustree/src/test/java/de/tuberlin/dima/dbt/exercises/bplustree/BPlusTreeTest.java
+++ bplustree/src/test/java/de/tuberlin/dima/dbt/exercises/bplustree/BPlusTreeTest.java
@@ -79,7 +79,7 @@ public class BPlusTreeTest {
 
     @Test
     public void splitLeafs() {
-        // given
+        // given (d=2)
         tree = newTree(newNode(keys(3),
                                nodes(newLeaf(keys(1, 2), values("a", "b")),
                                      newLeaf(keys(3, 4, 5, 6),
@@ -92,6 +92,248 @@ public class BPlusTreeTest {
                 nodes(newLeaf(keys(1, 2), values("a", "b")),
                       newLeaf(keys(3, 4), values("c", "d")),
                       newLeaf(keys(5, 6, 7), values("e", "f", "g")))))));
+    }
+    @Test
+    public void insertToEmptyRoot() {
+        // given
+        tree = newEmptyTree();
+        // when
+        tree.insert(2, "b");
+        // then
+        assertThat(tree, isTree(
+                newTree(newLeaf(keys(2), values("b")))));
+    }
+
+    @Test
+    public void insertInto2dExampleEmptyLeafNode() { // order of tree doesn't count root
+        // given
+        tree = newTree(newNode(keys(51),
+                nodes(newNode(keys(11,30),
+                                nodes(newLeaf(keys(2,7), values("2", "7")), newLeaf(keys(12,15,22), values("12", "15", "22")), newLeaf(keys(35,41), values("35", "41")))),
+                        newNode(keys(66,78),
+                                nodes(newLeaf(keys(53,54,63), values("53", "54","63")), newLeaf(keys(68,69,71,76), values("68", "69", "71", "76")), newLeaf(keys(79,84,93), values("79", "84", "93"))))
+                        )));
+        // when
+        tree.insert(57, "57");
+        // then
+        assertThat(tree, isTree(
+                newTree(newNode(keys(51),
+                        nodes(newNode(keys(11,30),
+                                        nodes(newLeaf(keys(2,7), values("2", "7")), newLeaf(keys(12,15,22), values("12", "15", "22")), newLeaf(keys(35,41), values("35", "41")))),
+                                newNode(keys(66,78),
+                                        nodes(newLeaf(keys(53,54,57,63), values("53", "54","57","63")), newLeaf(keys(68,69,71,76), values("68", "69", "71", "76")), newLeaf(keys(79,84,93), values("79", "84", "93"))))
+                        )))));
+    }
+
+    @Test
+    public void insertInto2dExampleFullLeafNodeRight() { // order of tree doesn't count root
+        // given
+        tree = newTree(newNode(keys(51),
+                nodes(newNode(keys(11,30),
+                                nodes(newLeaf(keys(2,7), values("2", "7")), newLeaf(keys(12,15,22), values("12", "15", "22")), newLeaf(keys(35,41), values("35", "41")))),
+                        newNode(keys(66,78),
+                                nodes(newLeaf(keys(53,54,63), values("53", "54","63")), newLeaf(keys(68,69,71,76), values("68", "69", "71", "76")), newLeaf(keys(79,84,93), values("79", "84", "93"))))
+                )));
+        // when
+        tree.insert(72, "72");
+        // then
+        assertThat(tree, isTree(
+                newTree(newNode(keys(51),
+                        nodes(newNode(keys(11,30),
+                                        nodes(newLeaf(keys(2,7), values("2", "7")), newLeaf(keys(12,15,22), values("12", "15", "22")), newLeaf(keys(35,41), values("35", "41")))),
+                                newNode(keys(66,71,78),
+                                        nodes(newLeaf(keys(53,54,63), values("53", "54","63")), newLeaf(keys(68,69), values("68", "69")), newLeaf(keys(71,72,76), values("71", "72", "76")), newLeaf(keys(79,84,93), values("79", "84", "93"))))
+                        )))));
+    }
+
+    @Test
+    public void insertInto2dExampleFullLeafNodeRightWithFullParents() {
+        // given
+        tree = newTree(newNode(keys(51),
+                nodes(newNode(keys(11,30),
+                                nodes(newLeaf(keys(2,7), values("2", "7")), newLeaf(keys(12,15,22), values("12", "15", "22")), newLeaf(keys(35,41), values("35", "41")))),
+                        newNode(keys(63,66,71,78),
+                                nodes(newLeaf(keys(53,54), values("53", "54")), newLeaf(keys(63,64,65), values("63","64","65")), newLeaf(keys(68,69), values("68", "69")), newLeaf(keys(71,72,76), values("71", "72", "76")), newLeaf(keys(79,84,93,94), values("79", "84", "93","94"))))
+                )));
+        // when
+        tree.insert(95, "95");
+        // then
+        assertThat(tree, isTree(
+                newTree(newNode(keys(51,71),
+                        nodes(newNode(keys(11,30),
+                                        nodes(newLeaf(keys(2,7), values("2", "7")), newLeaf(keys(12,15,22), values("12", "15", "22")), newLeaf(keys(35,41), values("35", "41")))),
+                                newNode(keys(63,66),
+                                        nodes(newLeaf(keys(53,54), values("53", "54")), newLeaf(keys(63,64,65), values("63","64","65")), newLeaf(keys(68,69), values("68", "69")))),
+                                newNode(keys(78, 93),
+                                        nodes(newLeaf(keys(71,72,76), values("71", "72", "76")), newLeaf(keys(79,84), values("79", "84")), newLeaf(keys(93,94,95), values("93","94","95"))))
+                        )))));
+    }
+
+    @Test
+    public void insertInto2dExampleFullLeafNodeRightWithFullRoot() { // TODO
+        // given
+        tree = newTree(newNode(keys(51,71,78),
+                nodes(newNode(keys(11,30),
+                                nodes(newLeaf(keys(2,7), values("2", "7")), newLeaf(keys(12,15,22), values("12", "15", "22")), newLeaf(keys(35,41), values("35", "41")))),
+                        newNode(keys(63,66),
+                                nodes(newLeaf(keys(53,54), values("53", "54")), newLeaf(keys(63,64,65), values("63","64","65")), newLeaf(keys(68,69), values("68", "69")))),
+                        newNode(keys(73, 75, 78, 93, 95),
+                                nodes(newLeaf(keys(71,72), values("71", "72")), newLeaf(keys(73,74), values("73","74")), newLeaf(keys(75,76,77), values("75","76","77")), newLeaf(keys(79,84), values("79", "84")), newLeaf(keys(93,94), values("93","94")), newLeaf(keys(95, 96, 97), values("95","96","97"))))
+                )));
+        // when
+        tree.insert(72, "72");
+        // then
+        assertThat(tree, isTree(
+                newTree(newNode(keys(51),
+                        nodes(newNode(keys(11,30),
+                                        nodes(newLeaf(keys(2,7), values("2", "7")), newLeaf(keys(12,15,22), values("12", "15", "22")), newLeaf(keys(35,41), values("35", "41")))),
+                                newNode(keys(66,71,78),
+                                        nodes(newLeaf(keys(53,54,63), values("53", "54","63")), newLeaf(keys(68,69), values("68", "69")), newLeaf(keys(71,72,76), values("71", "72", "76")), newLeaf(keys(79,84,93), values("79", "84", "93"))))
+                        )))));
+    }
+
+    @Test
+    public void insertInto2dExampleFullLeafNodeLeft() { // order of tree doesn't count root
+        // given
+        tree = newTree(newNode(keys(51),
+                nodes(newNode(keys(11,30),
+                                nodes(newLeaf(keys(2,7), values("2", "7")), newLeaf(keys(12,15,22,23), values("12", "15", "22", "23")), newLeaf(keys(35,41), values("35", "41")))),
+                        newNode(keys(66,78),
+                                nodes(newLeaf(keys(53,54,63), values("53", "54","63")), newLeaf(keys(68,69,71,76), values("68", "69", "71", "76")), newLeaf(keys(79,84,93), values("79", "84", "93"))))
+                )));
+        // when
+        tree.insert(26, "26");
+        // then
+        assertThat(tree, isTree(
+                newTree(newNode(keys(51),
+                        nodes(newNode(keys(11,22,30),
+                                        nodes(newLeaf(keys(2,7), values("2", "7")), newLeaf(keys(12,15), values("12", "15")), newLeaf(keys(22,23,26), values("22", "23", "26")), newLeaf(keys(35,41), values("35", "41")))),
+                                newNode(keys(66,78),
+                                        nodes(newLeaf(keys(53,54,63), values("53", "54","63")), newLeaf(keys(68,69,71,76), values("68", "69", "71", "76")), newLeaf(keys(79,84,93), values("79", "84", "93"))))
+                        )))));
+    }
+
+    @Test
+    public void insertInto3dExampleFullRoot() { // Has to split at root and tree becomes 1 dimension larger
+        // given
+        tree = newTree(
+                newNode(
+                        keys(30, 50, 81, 95), // Full root
+                        nodes(
+                                newNode(
+                                        keys(15, 25),
+                                        nodes(
+                                                newLeaf(keys(5, 10, 12, 14), values("5", "10", "12", "14")), // Full leaf node
+                                                newLeaf(keys(16, 18, 20, 22), values("16", "18", "20", "22")),
+                                                newLeaf(keys(26, 28), values("26", "28"))
+                                        )
+                                ),
+                                newNode(
+                                        keys(35, 45),
+                                        nodes(
+                                                newLeaf(keys(30, 32, 34), values("30", "32", "34")),
+                                                newLeaf(keys(36, 38, 40, 44), values("36", "38", "40", "44")),
+                                                newLeaf(keys(45, 46, 47, 48), values("45", "46", "47", "48"))
+                                        )
+                                ),
+                                newNode(
+                                        keys(60, 75),
+                                        nodes(
+                                                newLeaf(keys(51, 53, 55), values("51", "53", "55")),
+                                                newLeaf(keys(61, 63, 65, 68), values("61", "63", "65", "68")),
+                                                newLeaf(keys(76, 78, 79), values("76", "78", "80"))
+                                        )
+                                ),
+                                newNode(
+                                        keys(83, 88),
+                                        nodes(
+                                                newLeaf(keys(80, 81, 82), values("80", "81", "82")),
+                                                newLeaf(keys(83, 84, 85, 86), values("83", "84", "85", "86")),
+                                                newLeaf(keys(88, 89, 90), values("88", "89", "90"))
+                                        )
+                                ),
+                                newNode(
+                                        keys(96, 110, 150, 200), // Full inner node
+                                        nodes(
+                                                newLeaf(keys(91, 92, 93), values("91", "92", "93")),
+                                                newLeaf(keys(99, 100, 101, 102), values("99", "100", "101", "102")), // Full leaf node
+                                                newLeaf(keys(115, 116, 117), values("115", "116", "117")),
+                                                newLeaf(keys(160, 165, 169), values("160", "165", "169")),
+                                                newLeaf(keys(200, 209, 220, 230), values("200","209", "220", "230")) // Full leaf node
+                                        )
+                                )
+                        )
+                )
+        );
+        // when
+        tree.insert(210, "210");
+        // then
+        assertThat(tree, isTree(
+                newTree(
+                        newNode(keys(81),
+                                nodes(
+                                        newNode(
+                                                keys(30, 50),
+                                                nodes(
+                                                        newNode(
+                                                                keys(15, 25),
+                                                                nodes(
+                                                                        newLeaf(keys(5, 10, 12, 14), values("5", "10", "12", "14")),
+                                                                        newLeaf(keys(16, 18, 20, 22), values("16", "18", "20", "22")),
+                                                                        newLeaf(keys(26, 28), values("26", "28"))
+                                                                )
+                                                        ),
+                                                        newNode(
+                                                                keys(35, 45),
+                                                                nodes(
+                                                                        newLeaf(keys(30, 32, 34), values("30", "32", "34")),
+                                                                        newLeaf(keys(36, 38, 40, 44), values("36", "38", "40", "44")),
+                                                                        newLeaf(keys(45, 46, 47, 48), values("45", "46", "47", "48"))
+                                                                )
+                                                        ),
+                                                        newNode(
+                                                                keys(60, 75),
+                                                                nodes(
+                                                                        newLeaf(keys(51, 53, 55), values("51", "53", "55")),
+                                                                        newLeaf(keys(61, 63, 65, 68), values("61", "63", "65", "68")),
+                                                                        newLeaf(keys(76, 78, 79), values("76", "78", "80"))
+                                                                )
+                                                        )
+                                                )
+                                        ),
+                                        newNode(
+                                                keys(95, 200),
+                                                nodes(
+                                                        newNode(
+                                                                keys(83, 88),
+                                                                nodes(
+                                                                        newLeaf(keys(80, 81, 82), values("80", "81", "82")),
+                                                                        newLeaf(keys(83, 84, 85, 86), values("83", "84", "85", "86")),
+                                                                        newLeaf(keys(88, 89, 90), values("88", "89", "90"))
+                                                                )
+                                                        ),
+                                                        newNode(
+                                                                keys(96, 110), // Full inner node
+                                                                nodes(
+                                                                        newLeaf(keys(91, 92, 93), values("91", "92", "93")),
+                                                                        newLeaf(keys(99, 100, 101, 102), values("99", "100", "101", "102")),
+                                                                        newLeaf(keys(115, 116, 117), values("115", "116", "117"))
+
+                                                                )
+                                                        ),
+                                                        newNode(
+                                                                keys(200, 210),
+                                                                nodes(
+                                                                        newLeaf(keys(160, 165, 169), values("160", "165", "169")),
+                                                                        newLeaf(keys(200, 209), values("200","209")),
+                                                                        newLeaf(keys(210, 220, 230), values("210","220", "230"))
+                                                                )
+                                                        )
+                                                )
+                                        )
+                                )
+                        )
+        )));
     }
 
     ///// Deletion tests
